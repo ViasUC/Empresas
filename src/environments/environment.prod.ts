9f30582736c93751c084138bdf6ca13792f3d6e4
Tu Nombre <alfre_costas@hotmail.com>
2025-11-18 12:46:29 -0300

    Fix: Actualizar URL backend a AWS directo (Cloudflare caido - temporal)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.viasuc.com/api/v1', // URL del servidor REST en producción
-  graphqlUrl: 'https://api.viasuc.com/graphql', // URL del servidor GraphQL en producción
-  wsUrl: 'wss://api.viasuc.com/graphql', // WebSocket seguro para subscriptions
+  // TEMPORAL: URL directa de AWS (Cloudflare cayó - 18/11/2025)
+  apiUrl: 'http://viasuc-backend-env.eba-hehvbb97t.us-east-2.elasticbeanstalk.com/api/v1',
+  graphqlUrl: 'http://viasuc-backend-env.eba-hehvbb97t.us-east-2.elasticbeanstalk.com/graphql',
+  wsUrl: 'ws://viasuc-backend-env.eba-hehvbb97t.us-east-2.elasticbeanstalk.com/graphql',
   appName: 'VIASUC - Empresas',
   version: '1.0.0',
   
